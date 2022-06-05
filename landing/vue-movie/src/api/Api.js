eaf70653074837af9b8fba91dcf92e2048a33e48
Artemy Morozov <a.marozau@akdev.by>
2022-06-05 22:07:52 +0300

    MD Landing media requests 1.3 and fixs layout

--- landing/vue-movie/src/api/Api.js
+++ landing/vue-movie/src/api/Api.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-
-class Api {
-    constructor() {
-        this.axios = axios.create({
-            baseURL: '/api',
-            headers: {}
-        });
-    }
-}
-
-export default Api
+// import axios from "axios";
+//
+// class Api {
+//     constructor() {
+//         this.axios = axios.create({
+//             baseURL: '/api',
+//             headers: {}
+//         });
+//     }
+// }
+//
+// export default Api
--- landing/vue-movie/src/components/blocks/CallUs/CallUs.vue
+++ landing/vue-movie/src/components/blocks/CallUs/CallUs.vue
@@ -6,22 +6,23 @@
           h3.specialty-call-us__title Подать заявку
           .footer__call-us
             p.call-us__text Записаться на консультацию
-            form.call-us__form
-              input.form__input(
-                v-model="formName"
-                autocomplete="off"
-                type='text'
-                placeholder="Ваше имя"
-                :class="{'complete-input' : formName}"
-              )
-              input.form__input(
-                v-model="formPhone"
-                autocomplete="off"
-                type='text'
-                placeholder="Телефон"
-                :class="{'complete-input' : formPhone}"
-              )
-            button.invite__button(@click="sendData") Подать заявку
+            //form.call-us__form
+            //  input.form__input(
+            //    v-model="formName"
+            //    autocomplete="off"
+            //    type='text'
+            //    placeholder="Ваше имя"
+            //    :class="{'complete-input' : formName}"
+            //  )
+            //  input.form__input(
+            //    v-model="formPhone"
+            //    autocomplete="off"
+            //    type='text'
+            //    placeholder="Телефон"
+            //    :class="{'complete-input' : formPhone}"
+            //  )
+            //button.invite__button(@click="sendData") Подать заявку
+            a.invite__button(href="https://forms.gle/L6xxG79LJ6mkSvjw6") Подать заявку
           .footer__contacts
             .contacts__contact
               img.contact__img(:src="`../../../assets/images/phone.png`")
@@ -32,7 +33,7 @@
 </template>
 
 <script>
-import Email from "@/api/Email";
+// import Email from "@/api/Email";
 export default {
   name: "CallUs",
   data () {
@@ -41,22 +42,22 @@ export default {
       formPhone: ''
     }
   },
-  methods: {
-    sendData() {
-      const params = {
-        name: this.formName,
-        phone: this.formPhone
-      }
-      Email.sendMail(params)
-        .then(() => {
-          this.$toasted.success('Заявка отправлена. Ожидайте звонка')
-          this.formName = ''
-          this.formPhone = ''
-        })
-        .catch(() => {
-          this.$toasted.error('Произошла ошибка при отправке заявки')
-        })
-    }
-  }
+  // methods: {
+  //   sendData() {
+  //     const params = {
+  //       name: this.formName,
+  //       phone: this.formPhone
+  //     }
+  //     Email.sendMail(params)
+  //       .then(() => {
+  //         this.$toasted.success('Заявка отправлена. Ожидайте звонка')
+  //         this.formName = ''
+  //         this.formPhone = ''
+  //       })
+  //       .catch(() => {
+  //         this.$toasted.error('Произошла ошибка при отправке заявки')
+  //       })
+  //   }
+  // }
 }
 </script>
